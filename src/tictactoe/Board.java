394cf57e1fb45c1a95d481dacaf5e993a7256005
albytterc <37966752+albytterc@users.noreply.github.com>
2021-06-08 22:58:43 -0400

    Add files via upload

---
+++ src/tictactoe/Board.java
@@ -0,0 +1,254 @@
+package tictactoe;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
+public class Board implements Cloneable {
+    private char[][] board;
+    Player playerX;
+    Player playerO;
+
+    public Board() {
+        board = new char[3][3];
+
+        for (char[] chars : board) {
+            Arrays.fill(chars, ' ');
+        }
+    }
+
+    public void printBoard() {
+        System.out.println("---------");
+        for (char[] chars : board) {
+            System.out.print("| ");
+            for (char aChar : chars)
+                System.out.print(aChar + " ");
+
+            System.out.println("|");
+        }
+        System.out.println("---------");
+    }
+
+    public boolean aiMoveEasy() {
+        System.out.println("Making move level \"easy\"");
+        ArrayList<Coords> coordsAvailable = getAvailableCoords();
+        // Select a random index from the arraylist and add it to the board
+        return selectRandCoords(coordsAvailable);
+    }
+
+    private boolean selectRandCoords(ArrayList<Coords> coordsAvailable) {
+        Random rand = new Random();
+        int index = rand.nextInt(coordsAvailable.size());
+        Coords coord = coordsAvailable.get(index);
+        int down = coord.getDown();
+        int across = coord.getAcross();
+        return addToBoard(down + 1 + ", " + (across + 1));
+    }
+
+    private ArrayList<Coords> getAvailableCoords() {
+        ArrayList<Coords> coordsAvailable = new ArrayList<>();
+        // loop through the board and add all available spaces to the arraylist
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (!isFilled(i, j)) {
+                    coordsAvailable.add(new Coords(i, j));
+                }
+            }
+        }
+        return coordsAvailable;
+    }
+
+    public boolean aiMoveMedium() {
+        System.out.println("Making move level \"medium\"");
+        ArrayList<Coords> coordsAvailable = getAvailableCoords();
+        Coords emptySpot;
+        Coords stopWin;
+        if (playerO.isBot) {
+            emptySpot = winningMove(playerO);
+            stopWin = winningMove(playerX);
+        } else {
+            emptySpot = winningMove(playerX);
+            stopWin = winningMove(playerO);
+        }
+        if (emptySpot != null) {
+            return addToBoard(emptySpot.toString());
+        } else if (stopWin != null) {
+            return addToBoard(stopWin.toString());
+        }
+        return selectRandCoords(coordsAvailable);
+    }
+
+    public boolean aiMoveHard() {
+        System.out.println("Making move level \"hard\"");
+        Player player = playerX.isBot ? playerX : playerO;
+        return addToBoard(findBestMove(this, player).toString());
+    }
+
+    public Coords findBestMove(Board b, Player player) {
+        Coords move = null;
+        ArrayList<Coords> freeMoves = b.getAvailableCoords();
+
+        int bestScore = Integer.MIN_VALUE;
+        for (Coords coords : freeMoves) {
+            b.board[coords.getDown()][coords.getAcross()] = player.getTypeChar();
+            int score = minimax(b, getOpponent(player), coords, false, player, 1);
+            if (score > bestScore) {
+                bestScore = score;
+                move = coords;
+            }
+            b.board[coords.getDown()][coords.getAcross()] = ' ';
+        }
+
+//        if (playerX.isBot) {
+//            int bestScore = Integer.MIN_VALUE;
+//            for (Coords coords : freeMoves) {
+//                b.board[coords.getDown()][coords.getAcross()] = 'X';
+//                int score = minimax(b, player, coords);
+//                if (bestScore < score) {
+//                    bestScore = score;
+//                    move = coords;
+//                }
+//                b.board[coords.getDown()][coords.getAcross()] = ' ';
+//            }
+//        } else {
+//            int bestScore = Integer.MAX_VALUE;
+//            for (Coords coords : freeMoves) {
+//                b.board[coords.getDown()][coords.getAcross()] = 'O';
+//                int score = minimax(b, player, coords);
+//                if (bestScore > score) {
+//                    bestScore = score;
+//                    move = coords;
+//                }
+//                b.board[coords.getDown()][coords.getAcross()] = ' ';
+//            }
+//        }
+        return new Coords(move.getDown() + 1, move.getAcross() + 1);
+    }
+
+    public int minimax(Board b, Player player, Coords move, boolean isMaximizing, Player startPlayer, int depth) {
+        if (b.gameOver(player, move)) {
+            if (b.playerX.isWinner()) {
+                return startPlayer.getTypeChar() == 'X' ? 10 - depth : depth - 10;
+            } else if (b.playerO.isWinner()) {
+                return startPlayer.getTypeChar() == 'O' ? 10 - depth : depth - 10;
+            } else {
+                return 0;
+            }
+
+//            if (b.playerX.isWinner() && b.playerX.isBot || b.playerO.isWinner() && b.playerO.isBot) {
+//                return 10;
+//            } else if (b.playerX.isWinner() || b.playerO.isWinner()) {
+//                return -10;
+//            } else {
+//                return 0;
+//            }
+        }
+
+        ArrayList<Coords> emptySpots = b.getAvailableCoords();
+        int bestScore = isMaximizing ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+        for (Coords coords : emptySpots) {
+            b.board[coords.getDown()][coords.getAcross()] = player.getTypeChar();
+            int score = minimax(b, getOpponent(player), coords, !isMaximizing, startPlayer, depth + 1);
+            bestScore = isMaximizing ? Math.max(bestScore, score) : Math.min(bestScore, score);
+            b.board[coords.getDown()][coords.getAcross()] = ' ';
+        }
+        return bestScore;
+    }
+
+    private Coords winningMove(Player player) {
+        // Find all available moves
+        // Play each move and check if the move leads to a win
+            // if it does return the coords of the move
+            // otherwise reset the board and try the next move
+
+        for (Coords move : getAvailableCoords()) {
+            int down = move.getDown();
+            int across = move.getAcross();
+            board[down][across] = player.getTypeChar();
+            boolean winningMoveFound = gameOver(player, move);
+            board[down][across] = ' ';
+            player.setWinner(false);
+            if (winningMoveFound) return new Coords(down + 1, across + 1);
+        }
+        return null;
+    }
+
+    public boolean addToBoard(String coords) {
+        Coords nums = processCoords(coords);
+        int down = nums.getDown();
+        int across = nums.getAcross();
+
+        Player player;
+        if (playerX.getMoveCount() == playerO.getMoveCount()) {
+            player = playerX;
+        } else {
+            player = playerO;
+        }
+
+        player.playMove();
+        board[down][across] = player.getTypeChar();
+
+        printBoard();
+        return gameOver(player, nums);
+    }
+
+    public boolean isFilled(int down, int across) {
+        return board[down][across] != ' ';
+    }
+
+
+    public boolean gameOver(Player player, Coords lastMove) {
+        int down = lastMove.getDown();
+        int across = lastMove.getAcross();
+        if (board[down][0] == board[down][1] && board[down][1] == board[down][2]) {
+            player.setWinner(true);
+            return true;
+        } else if (board[0][across] == board[1][across] && board[1][across] == board[2][across]) {
+            player.setWinner(true);
+            return true;
+        }
+
+        if (down == across) {
+            if (board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
+                player.setWinner(true);
+                return true;
+            }
+        }
+
+        if (down + across == 2) {
+            if (board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
+                player.setWinner(true);
+                return true;
+            }
+        }
+
+        // No winners yet; check if board is full
+        for (char[] row : board) {
+            for (char spot : row) {
+                if (spot == ' ') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    // Converts the coordinates string given in the prompt to two integers
+    // Returns an array of the two numbers
+    private Coords processCoords(String coords) {
+        String[] parts = coords.split("[ ,]+");
+        String first = parts[0];
+        String second = parts[1];
+        int down = Integer.parseInt(first);
+        int across = Integer.parseInt(second);
+        return new Coords(down - 1, across - 1);
+    }
+
+    private Player getOpponent(Player player) {
+        if (player.getTypeChar() == 'X') {
+            return playerO;
+        }
+        return playerX;
+    }
+
+}
---
+++ src/tictactoe/TextUI.java
@@ -0,0 +1,190 @@
+package tictactoe;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class TextUI {
+    private final Scanner scanner;
+    private Board board;
+
+    public TextUI(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public void init() {
+        while (true) {
+            board = new Board();
+            System.out.print("Input command: > ");
+            String command = scanner.nextLine();
+            if (command.equalsIgnoreCase("exit")) {
+                scanner.close();
+                return;
+            }
+
+            String[] parts = command.split(" ");
+            ArrayList<String> modes = new ArrayList<>();
+            Collections.addAll(modes, "user", "easy", "medium", "hard");
+            if (parts.length != 3 ||
+                    !parts[0].equalsIgnoreCase("start") ||
+                    !modes.contains(parts[1]) || !modes.contains(parts[2])
+            ) {
+                System.out.println("Bad parameters!");
+            } else {
+                board.printBoard();
+                startGame(parts[1], parts[2]);
+            }
+        }
+
+    }
+
+    public void startGame(String xPlayer, String oPlayer) { // select the mode from the string and pass it to the playMoves methods
+        if (xPlayer.equalsIgnoreCase("user") && oPlayer.equalsIgnoreCase("user")) {
+            board.playerX = new Player(PlayerType.X, false);
+            board.playerO = new Player(PlayerType.O, false);
+            usersPlayMoves();
+        } else if (!xPlayer.equalsIgnoreCase("user") && !oPlayer.equalsIgnoreCase("user")) {
+            board.playerX = new Player(PlayerType.X, true);
+            board.playerO = new Player(PlayerType.O, true);
+            botsPlayMoves(xPlayer, oPlayer);
+        } else if (xPlayer.equalsIgnoreCase("user")) {
+            board.playerX = new Player(PlayerType.X, false);
+            board.playerO = new Player(PlayerType.O, true);
+            userAndBotPlayMoves(board.playerX, oPlayer);
+        } else {
+            board.playerX = new Player(PlayerType.X, true);
+            board.playerO = new Player(PlayerType.O, false);
+            userAndBotPlayMoves(board.playerX, xPlayer);
+        }
+        System.out.println(printOutcome());
+    }
+
+    private String printOutcome() {
+        if (board.playerX.isWinner()) {
+            return "X wins";
+        } else if (board.playerO.isWinner()) {
+            return "O wins";
+        }
+
+        return "Draw";
+    }
+
+    private void usersPlayMoves() {
+        boolean gameOver = false;
+        while (!gameOver) {
+            System.out.print("Enter the coordinates: > ");
+            String coords = scanner.nextLine();
+            if (validCoords(coords)) {
+                gameOver = board.addToBoard(coords);
+            }
+        }
+    }
+
+    private void userAndBotPlayMoves(Player player1, String mode) {
+        boolean gameOver = false;
+        while (!gameOver) {
+            if (player1.isBot) { // X is bot, O is user
+                if (mode.equalsIgnoreCase("easy")) {
+                    gameOver = board.aiMoveEasy();
+                } else if (mode.equalsIgnoreCase("medium")) {
+                    gameOver = board.aiMoveMedium();
+                } else {
+                    gameOver = board.aiMoveHard();
+                }
+
+                if (!gameOver) {
+                    String coords;
+                    do {
+                        System.out.print("Enter the coordinates: > ");
+                        coords = scanner.nextLine();
+                    } while (!validCoords(coords));
+
+                    gameOver = board.addToBoard(coords);
+                }
+            } else { // X is user, O is bot
+                String coords;
+                do {
+                    System.out.print("Enter the coordinates: > ");
+                    coords = scanner.nextLine();
+                } while (!validCoords(coords));
+
+                gameOver = board.addToBoard(coords);
+                if (gameOver) break;
+                if (mode.equalsIgnoreCase("easy")) {
+                    gameOver = board.aiMoveEasy();
+                } else if (mode.equalsIgnoreCase("medium")) {
+                    gameOver = board.aiMoveMedium();
+                } else {
+                    gameOver = board.aiMoveHard();
+                }
+            }
+        }
+    }
+
+    private void botsPlayMoves(String modeX, String modeO) {
+        boolean gameOver = false;
+        while (!gameOver) {
+            if (modeX.equalsIgnoreCase("easy")) {
+                gameOver = board.aiMoveEasy();
+            } else if (modeX.equalsIgnoreCase("medium")) {
+                gameOver = board.aiMoveMedium();
+            } else {
+                gameOver = board.aiMoveHard();
+            }
+
+            if (gameOver) break;
+
+            if (modeO.equalsIgnoreCase("easy")) {
+                gameOver = board.aiMoveEasy();
+            } else if (modeO.equalsIgnoreCase("medium")) {
+                gameOver = board.aiMoveMedium();
+            } else {
+                gameOver = board.aiMoveHard();
+            }
+        }
+    }
+
+    public boolean validCoords(String coords) {
+        String[] parts = coords.split("[, ]+");
+        String first;
+        String second;
+
+        try {
+            first = parts[0];
+            second = parts[1];
+        } catch (Exception e) {
+            System.out.println("You should enter numbers!");
+            return false;
+        }
+
+        int down;
+        int across;
+
+        try {
+            down = Integer.parseInt(first);
+            across = Integer.parseInt(second);
+        } catch (Exception e) {
+            System.out.println("You should enter numbers!");
+            return false;
+        }
+
+        if ((down > 3 || down < 1) || (across > 3 || across < 1)) {
+            System.out.println("Coordinates should be from 1 to 3!");
+            return false;
+        }
+
+        if (board.isFilled(down - 1, across - 1)) {
+            System.out.println("This cell is occupied! Choose another one!");
+            return false;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        TextUI tUI = new TextUI(scanner);
+        tUI.init();
+
+    }
+}
